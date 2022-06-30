f777f51e74cdfabb9bb03249a087b733f6aa92c5
fitunabu <fitunabu@gmail.com>
2022-06-30 05:56:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - Starting point
-*Return: Always 0     
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("\n Programming is like building a multilingual puzzle");
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+
