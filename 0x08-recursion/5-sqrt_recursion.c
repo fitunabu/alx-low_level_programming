d24236348f6e7c6ecdb12b964732c9d2aae67261
fitunabu <fitunabu@gmail.com>
2022-07-20 05:22:41 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,7 +15,7 @@ if (end >= start)
 mid = start + (end - start) / 2;
 if (mid * mid == m)
 return (mid);
-/* following binary search */
+
 if (mid * mid > m)
 return (sqtRecursive(start, mid - 1, m));
 if (mid * mid < m)
@@ -24,10 +24,10 @@ return (sqtRecursive(mid + 1, end, m));
 return (-1);
 }
 /**
-*  * _sqrt_recursion - finds the natural square root of a number
-*   * @n: given number
-*    * Return: square root of n or -1
-*     **/
+* _sqrt_recursion - finds the natural square root of a number
+* @n: given number
+* Return: square root of n or -1
+*/
 int _sqrt_recursion(int n)
 {
 if (n < 0)
