f21e3702867a582816d18a09caa684a7b88f0768
fitunabu <fitunabu@gmail.com>
2022-07-22 05:12:51 -0700

    multiply of numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
-* main - a program that multiply two numbers
-* @argc: arguement count
-* @argv: arguement value
-* Return: result of multiplication or 1
-*/
-int main(int argc, char *argv[])
-{
-int a, b, result;
 
-if (argc <= 2)
+/**
+* main - A program that multiplies two numbers
+* @argc: The arguments' counter
+* @argv: The arguments' values
+* Return: Always 0 (Success)
+*/
+int main(int argc, char **argv)
+{
+int num1, num2, result;
+
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-result = a * b;
+else
+{
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
+}
 }
