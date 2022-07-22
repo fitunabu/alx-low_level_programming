d702e3f29e590d9b948371e9dd9254b5e8159dc1
fitunabu <fitunabu@gmail.com>
2022-07-22 00:48:24 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -  program prints its name
+* @argc: arguement count
+* @argv: arrgument value
+* Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+argc = argc;
+printf("%s\n", *argv);
+return (0);
+}
