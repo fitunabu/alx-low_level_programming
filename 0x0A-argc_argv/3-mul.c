6f1bf7799b856115a4b60a02e55c32c8fec0428a
fitunabu <fitunabu@gmail.com>
2022-07-22 07:30:26 -0700

    multiply of numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
-* main - mul 2 argv
-* @argc:int
-* @argv:string
-* Return:int
+* main - print product of argument numbers
+* @argc: argument counter
+* @argv: numbers to multiply
+* Return: 0 on success, 1 if two arguments not given
 */
+
 int main(int argc, char *argv[])
 {
-int x, i, j;
 
+/* validate input */
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-else
-{
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-x = i * j;
-printf("%d\n", x);
-}
+
+/* mulitply two arguments passed via cmd line */
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
