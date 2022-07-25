5188ee520b75116744b5b26049bd34304659fbfa
fitunabu <fitunabu@gmail.com>
2022-07-25 00:52:47 -0700

    concatenates all the arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,70 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* _strlen - returns the length of a string
-* @s: string
-* Return: length
+* argstostr - A function that concatenates all arguments of your program
+* @ac: number of arguments
+* @av: array containing arguments
+* Return: A pointer to string that containing all arguments
+* or NULL if ac == 0, if av == NULL, or upon failure
 */
-
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-len++, s++;
-
-return (len);
-}
-
-/**
-* argstostr - concatenates all the arguments of your program
-* @ac: argc
-* @av: arguments
-* Return: pointer to array
-*/
-
 char *argstostr(int ac, char **av)
 {
-char *s;
-int len = 0, i, j, k = 0;
+char *new_str;
+int len = 0, i = 0, j, k = 0;
 
-if (ac == 0 || av == NULL) /* validate input */
+if (ac <= 0 || av == NULL)
 return (NULL);
 
-/* find length to malloc */
+for (; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+len++;
+len++;
+}
+
+len++;
+new_str = malloc(len * sizeof(char));
+if (new_str == NULL)
+return (NULL);
 
 for (i = 0; i < ac; i++)
 {
-len += _strlen(av[i]);
-}
-
-len += (ac + 1); /* add space for newlines and null terminator */
-
-/* allocate memory and free if error */
-
-s = malloc(len * sizeof(char));
-
-if (s == NULL)
+for (j = 0; av[i][j]; j++)
 {
-free(s);
-return (NULL);
+new_str[k] = av[i][j];
+k++;
+}
+new_str[k] = '\n';
+k++;
 }
 
-/* insert each arg into *str */
-
-for (i = 0; i < ac; i++)
-{
-for (j = 0; j < _strlen(av[i]); j++)
-{
-s[k++] = av[i][j];
-}
-
-s[k++] = '\n';
-
-}
-
-return (s);
-
+new_str[k] = '\0';
+return (new_str);
 }
